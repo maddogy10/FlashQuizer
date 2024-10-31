7dba94871a4062dc75c14c9ee04f37ac714d3377
maddogy10 <90865376+maddogy10@users.noreply.github.com>
2024-10-31 06:40:11 -0400

    Add files via upload

---
+++ wordSearch.js
@@ -0,0 +1,138 @@
+const grid = document.getElementById('grid')
+const wordList = document.getElementById('wordList')
+const size = 10;
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const gridArray = Array(size).fill().map(() => Array(size).fill(''));
+const foundWords = new Set();
+let word = "";
+let selectedWords = []
+const wordSearchButton = document.getElementById('wordSearch')
+/* 
+wordSearchButton.addEventListener('click', function () {
+    fillGrid();
+})
+*/
+
+
+function runWordSearch() {
+
+    let i = 0;
+    retrieved.forEach((e) => {
+        retrieved[i] = e.toUpperCase()
+        i++;
+    })
+    const allWords = retrieved;
+    console.log(retrieved)
+
+while (selectedWords.length < 5) {
+    word = allWords[Math.floor(Math.random() * allWords.length)];
+    if(!selectedWords.includes(word)) {
+        selectedWords.push(word)
+    }
+    
+}
+fillGrid()
+document.querySelectorAll('.cell').forEach(cell => {
+    
+    cell.addEventListener('click', () => {
+        console.log("click")
+        let input = prompt('Enter the word you found:').toUpperCase();
+        if (selectedWords.includes(input) && !foundWords.has(input)) {
+            highlightWord(input)
+        } else {
+            alert('Word not found or already found!')
+        }
+    })
+})
+
+console.log(selectedWords)
+   
+
+
+}
+function placeWord(word) {
+    let placed = false;
+    console.log(placed)
+    while (!placed) {
+        const startX = Math.floor(Math.random() * (size - word.length + 1))
+        const startY = Math.floor(Math.random() * size)
+        let canPlace = true
+
+        for (let i = 0; i < word.length; i++) {
+            if (gridArray[startY][startX + i] !== '') {
+                canPlace = false;
+                break;
+            }
+        }
+            if (canPlace) {
+                for (let i = 0; i < word.length; i++) {
+                    gridArray[startY][startX + i] = word[i]
+                }
+                placed = true;
+            }
+        
+    }
+}
+function fillGrid() {
+    console.log("fill")
+    selectedWords.forEach(word => {
+        placeWord(word);
+        const li = document.createElement('li')
+        li.textContent = word;
+        li.dataset.word = word;
+        wordList.appendChild(li);
+    })
+
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            if(!gridArray[y][x]) {
+                gridArray[y][x] = letters[Math.floor(Math.random() * letters.length)]
+            }
+            const cell = document.createElement('div')
+            cell.classList.add('cell')
+            cell.textContent = gridArray[y][x]
+            grid.appendChild(cell)
+        }
+    }
+}
+function highlightWord(word) {
+    let startX = -1;
+    let startY = -1;
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            if (gridArray[y][x] === word[0]) {
+                let match = true;
+                for (let i = 0; i < word.length; i++) {
+                    if (gridArray[y][x+i] !== word[i]) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    startX = x;
+                    startY = y;
+                    break;
+                }
+            }
+        }
+        if (startX !== -1) break;
+    }
+    if (startX !== -1) {
+        for (let i = 0; i < word.length; i++) {
+            const index = startY * size + startX + i;
+            grid.children[index].classList.add('found');
+        }
+        document.querySelector(`[data-word="${word}"]`).classList.add('strikethrough');
+        foundWords.add(word);
+        if (foundWords.size === selectedWords.length) {
+            console.log( document.getElementById('congratsModal'))
+
+            document.getElementById('congratsModal').style.display = 'block'
+            document.body.classList.add('blur')
+        }
+    }
+}
+
+
+
+
---
+++ write.js
@@ -0,0 +1,249 @@
+// next: work on the bars on the write w fractions
+const termBox = document.getElementById("writegiven")
+const writeInput = document.getElementById("writeInput")
+const userWrote = document.getElementById("userwrote")
+const correctAns = document.getElementById("correctAns")
+const rightWrongWrite = document.getElementById("rightWrongWrite")
+const contButton = document.getElementById("contButton")
+const remLeft = document.getElementById("remleft")
+const incLeft = document.getElementById("incleft")
+const corLeft = document.getElementById("corleft")
+const rightOrNot = document.getElementById('rightornot')
+let termArrNum = [];
+let defArrNum = [];
+let numTerms;
+let remainingNum = document.getElementById("remainingNum")
+let incorrectNum = document.getElementById("incorrectNum")
+let correctNum = document.getElementById("correctNum")
+//original term and definition storage
+let termArr = []
+let defArr = []
+let x;
+let remNum;
+let incNum = 0;
+let corrNum = 0;
+let remBar;
+let incBar;
+let corrBar;
+let remBarCover = document.getElementById("remleft")
+let incBarCover = document.getElementById("incleft")
+let corrBarCover = document.getElementById("corleft")
+//let wrongTermArr = []
+//let wrongTermDef = []
+// number of term/def for write order
+let ogTermArr = []
+let ogDefArr = []
+function termWrite() {
+  contButton.innerHTML = "Continue"
+    x = 0;
+  numTerms = 100 / retrieved.length
+  numTerms= retrieved.length
+  console.log(retrieved)
+  console.log(numTerms);
+  remNum = numTerms;
+  
+  remainingNum.innerHTML = remNum;
+  remBar = 100;
+  remBarCover.style.width = remBar + "%";
+  corrBar = 0;
+  incBar = 0;
+  //do the bars
+ /*
+  let termArrNum = []
+ let randTermArr = []
+ for (i = 0; i < numOfTerms; i++) {
+  randTermArr[i] = i
+ }
+ 
+  for(j = 0; j < retrieved.length; j++ ) {
+   /* 
+  console.log(randTermArr)
+  if (randTermArr.includes(randTerm)!=true) {
+    let i = 0
+    termArrNum[i] = retrieved[randTerm]
+    i++
+  }*/
+  // while i < retrieved.length
+ 
+  for(i =0; i < retrieved.length; i++) {
+    termArr[i] = i;
+    defArr[i] = i;
+   
+  }
+  let j = 0;
+  //decides order
+  while (termArr.length !=0) {
+  
+  
+  let randTerm = Math.floor(Math.random()*retrieved.length)
+  //  randTermArr[j] = randTerm
+    if (termArr.includes(randTerm)) {
+      termArrNum[j] = retrieved[randTerm]
+      defArrNum[j] = gotDefs[randTerm]
+     // console.log(termArr)
+     // console.log(termArrNum)
+     
+     for (i=0; i < termArr.length; i++) {
+      if(termArr[i] == randTerm) {
+        termArr.splice(i, 1)
+        console.log("I spliced " + i)
+     //   console.log(termArr)
+      }
+     }
+      j++
+    }
+   
+   
+  
+
+ // defArr = termArr
+  
+  //find array value where rand num is drawn and remove from array
+  }
+  //make def array
+  //implement array into html
+  for (let i=0; i < termArrNum.length; i++) {
+    ogTermArr[i] = termArrNum[i];
+    ogDefArr[i] = defArrNum[i];
+    console.log(ogTermArr)
+    console.log(ogDefArr)
+  }
+ /*  ogTermArr = termArrNum
+    ogDefArr = defArrNum*/
+  termBox.innerHTML = termArrNum[x];
+  }
+
+
+
+function checkWrite() {
+  console.log("enter")
+  console.log(x)
+  console.log(termArr.length)
+  console.log(defArrNum[x])
+  //if correct and inside array length
+ if (writeInput.value == defArrNum[x] && x <= termArrNum.length) {
+    corrNum += 1;
+    correctNum.innerHTML = corrNum;
+    remNum -=1;
+    remainingNum.innerHTML = remNum;
+    console.log(numTerms);
+    remBar -= (100* 1 / numTerms)
+    if (remBar < 0) {
+      remBar = 0;
+    }
+    remBarCover.style.width = remBar + "%";
+    corrBar += 100*1/numTerms;
+    corrBarCover.style.width = corrBar +"%";
+    rightWrongWrite.style.display = "block"
+    userWrote.style.display = "block"
+    userWrote.innerHTML = writeInput.value
+    userWrote.style.color = "green"
+  rightOrNot.innerHTML = "Correct!"
+    correctAns.innerHTML = defArrNum[x];
+    correctAns.style.color = "green"
+     // pop last element of the array
+    if (ogTermArr.length > 0) {
+      if (x < ogTermArr.length - 1) {
+        ogTermArr.splice(x,1)
+      ogDefArr.splice(x,1)
+      console.log("spliced")
+      } else if (x >= ogTermArr.length -1) {
+        ogTermArr.pop()
+        ogDefArr.pop()
+    } else {
+      ogTermArr[0] = null
+      ogDefArr[0] = null
+    }
+  }
+    console.log(ogTermArr)
+    console.log(ogDefArr)
+    console.log(defArrNum)
+   // remLeft.style.width -= numTerms
+   
+    x+=1;
+    //do it with numbers instead of copying array
+    termBox.innerHTML = termArrNum[x];
+    writeInput.value = ""
+// if wrong
+  } else if (writeInput.value != defArrNum[x]  && x < termArrNum.length) {
+    incNum += 1;
+    incorrectNum.innerHTML = incNum;
+    remNum -=1;
+    remainingNum.innerHTML = remNum;
+    remBar -= (100* 1 / numTerms)
+    if (remBar < 0) {
+      remBar = 0;
+      console.log("less")
+    }
+    remBarCover.style.width = remBar + "%";
+    incBar += 100*1/numTerms;
+    incBarCover.style.width = incBar +"%";
+    console.log("no")
+    rightWrongWrite.style.display = "block"
+    userWrote.innerHTML = writeInput.value
+    userWrote.style.color = "red"
+    rightOrNot.innerHTML = "Incorrect :("
+    correctAns.innerHTML = defArrNum[x];
+    correctAns.style.color = "green"
+    x+=1;
+    termBox.innerHTML = termArrNum[x];
+    writeInput.value = ""
+ }
+ 
+}
+function closeCorrWrite() {
+//make sure this functions correctly
+  if (x <= termArrNum.length -1) {
+  rightWrongWrite.style.display = "none"
+  console.log("close")
+  } else if (x > termArrNum.length - 1) {
+    
+    if (ogTermArr.length == 0) {
+      // rightWrongWrite.style.display = "block"
+    //userWrote.style.display = "none"
+    //change continue button to restart round
+      contButton.innerText = "Restart"
+      console.log(contButton.innerText)
+      corrNum = 0;
+      //termArrNum = ogTermArr
+    //  defArrNum = ogDefArr
+      termWrite()
+    } else {
+     
+      for (let i = 0; i < termArrNum.length; i++) {
+        /*
+        if (!ogTermArr.includes(i)) {
+          console.log(termArrNum[i])
+          termArrNum.splice(i,1)
+          defArrNum.splice(i,1)
+        }
+       
+*/
+        termArrNum = ogTermArr
+        defArrNum = ogDefArr
+        console.log(termArrNum)
+        console.log(defArrNum)
+        contButton.innerText = "Restart"
+        contButton.addEventListener('click', () => {
+          x=0;
+          termBox.innerHTML = termArrNum[x]
+          writeInput.value = ''
+          rightWrongWrite.style.display = "none"
+        })
+        // figure out where to put these
+        
+        console.log(numTerms);
+    remNum = numTerms;
+    remBar = 100;
+    remBarCover.style.width = remBar + "%";
+    incBar = 0;
+    incNum = 0;
+    incBarCover.style.width = incBar + "%";
+      }
+    }
+   // let x > termArrNum.length - 1 && ogTermArr[0] == null
+   // allow it to restart
+   }
+}
+
+
